266afcb9b02e729dd573b0d3f3400aa03a90aca3
piyan1212 <piyanstefani@gmail.com>
2024-08-03 04:24:41 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from telebot import TeleBot
+import bypass
+
+bot = TeleBot('6682279482:AAHyfXCzAYIgrPDNfq9y6gU4_oTDfDk-i8Q')
+
+
+@bot.message_handler(commands=['start'])
+def generator(message):
+    bot.reply_to(message, "Silahkan Kirim Linknya")
+@bot.message_handler(content_types="text")
+def generator(message):
+    try:
+        data = bypass.ouo_bypass(message.text)
+        bot.reply_to(message, text=f'<code>{data}</code>', parse_mode="HTML")
+    except:
+        bot.reply_to(message, "Error")
+
+
+bot.polling()
